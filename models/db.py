355ef4c5cd8ce0d73d88c63c8e233628efa6bd97
Allan Guan <forevaufo@gmail.com>
2015-10-11 16:10:22 -0500

    erased class / standard table from db

--- models/db.py
+++ models/db.py
@@ -92,39 +92,3 @@ auth.settings.reset_password_requires_verification = True
 
 ## after defining tables, uncomment below to enable auditing
 # auth.enable_record_versioning(db)
-
-
-
-db = DAL("sqlite://storage.sqlite")
-
-db.define_table(
-    'classes',
-    Field('name'),
-    Field('gradeLevel', 'integer'),
-    Field('startDate', 'integer'),
-    Field('endDate', 'integer'),
-    # studentList Obj
-    Field('studentList'),
-    # grade Obj
-    Field('grade'),
-    # content area Obj
-    Field('content_area'),
-    format = '%(name)s')
-
-db.classes.name.requires = IS_NOT_EMPTY()
-db.classes.gradeLevel.requires = IS_NOT_EMPTY()
-db.classes.startDate.requires = IS_NOT_EMPTY()
-db.classes.endDate.requires = IS_NOT_EMPTY()
-db.classes.id.readable = db.classes.id.writable = False
-
-
-db.define_table(
-    'standards',
-    Field('refNum'),
-    Field('shortName'),
-    Field('description'),
-    format = '%(shortName)s')
-
-db.standards.refNum.requires = IS_NOT_EMPTY()
-db.standards.shortName.requires = IS_NOT_EMPTY()
-db.standards.description.requires = IS_NOT_EMPTY()
