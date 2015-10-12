e7326bcdd82de26accdcddd7d9310f86ae70c29f
Bryan Patzke <projects@2bpencil.com>
2015-10-12 16:54:32 -0500

    Merged changes from master repository.

---
+++ controllers/class.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# try something like
+#def index(): return dict(message="hello from create.py")
+
+def index():
+    classes = SQLFORM.smartgrid(db.classList)
+    return dict(classes=classes)
+
+# def index():
+#     classList = SQLFORM(db.classList)
+#     return dict(classList=classList)
+
+# def index():
+#     classes = db().select(db.classes.id, db.classes.name, orderby=db.classes.name)
+#     return dict(classes=classes)
+
+
+# def create():
+#     form = SQLFORM(db.classes).process(next=URL('index'))
+#     return dict(form=form)
+
+# def show():
+#     grid = SQLFORM.grid(db.classes.id)
+#     return dict(grid=grid)
+
+# def edit():
+#     this_class = db.classes(request.args(0,cast=int)) or redirect(URL('index'))
+#     form = SQLFORM(db.classes, this_class).process(next= URL('show', args=request.args))
+#     return dict(form=form)
