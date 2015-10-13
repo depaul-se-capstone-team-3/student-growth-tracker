cef573f2990172bee0f2ec1ddfcd674643c25bb5
Bryan Patzke <projects@2bpencil.com>
2015-10-12 19:20:39 -0500

    Updated

--- models/db_40_gradebook.py
+++ models/db_40_gradebook.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-# db.define_table(
-#     'gradebook',
-#     #needs additional validator to check if in DB.
-#     Field('class', 'reference class'))
+
+db.define_table(
+    'gradebook',
+    #needs additional validator to check if in DB.
+    Field('class', 'reference class'))
