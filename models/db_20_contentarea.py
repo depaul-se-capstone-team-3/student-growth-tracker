c857dcee378f9ad38fbbe8b8a4c3d16bffa4e998
Larry Graves <lawrence.graves86@gmail.com>
2015-10-14 18:38:43 -0500

    removed reference to standard from contentarea

--- models/db_20_contentarea.py
+++ models/db_20_contentarea.py
@@ -4,4 +4,4 @@ db.define_table(
     'contentarea',
     Field('name', required=True, requires=IS_NOT_EMPTY()),
     #needs additional validator to check if in DB.
-    Field('standard', 'reference standard'))
+    )
