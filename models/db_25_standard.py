cfe5ff3d7ca5b27e773f72205a661ca3ee3862f3
Larry Graves <lawrence.graves86@gmail.com>
2015-10-16 20:48:24 -0500

    many various changes to validators in models, as well as some formats, to get imported values to work properly.  Many errors encountered, and resolved.

--- models/db_25_standard.py
+++ models/db_25_standard.py
@@ -2,9 +2,9 @@
 
 db.define_table(
     'standard',
-    Field('reference_number', required=True, requires=IS_NOT_EMPTY),
-    Field('short_name', required=True, requires=IS_NOT_EMPTY),
-    Field('description', required=True, requires=IS_NOT_EMPTY),
+    Field('reference_number', required=True),
+    Field('short_name', required=True),
+    Field('description', required=True),
     Field('content_area', 'reference contentarea'),
     format = '%(shortName)s')
 
