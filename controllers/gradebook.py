d01adaf600f180a936e46789ccfa75b113afa0cf
Larry Graves <lawrence.graves86@gmail.com>
2015-10-18 16:59:28 -0500

    added filtering functionality to gradebook, and added views for gradebook

--- controllers/gradebook.py
+++ controllers/gradebook.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 # try something like
 @auth.requires_login()
-def index(): 
-    grid = SQLFORM.smartgrid(db.gradebook)
-    return dict(grid=grid)
-    #return locals()
+#def index():
+#    query = db.gradebook.teacher == auth.user_id
+#    gradebook = db(query)
+#    return dict(message='Class List - %(first_name)s' % auth.user,gradebook=gradebook)
+def index():
+    constraints = db.gradebook.teacher == auth.user.id
+    grid = SQLFORM.smartgrid(db.gradebook,constraints={'gradebook':(db.gradebook.teacher==auth.user_id)},links_in_grid=False)
+    return dict(message='Class List - %(first_name)s' % auth.user,grid=grid)
 
 def create():
     form = SQLFORM(db.gradebook).process(next=URL('index'))
-    return dict(form=form)
+    return dict(message='Add Class to %(first_name)s Gradebook' % auth.user,form=form)
