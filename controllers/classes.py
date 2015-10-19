7b3f3ebc35866f1230fbc11cd34c361dbe6be85c
Allan Guan <forevaufo@gmail.com>
2015-10-18 19:22:39 -0500

    added view for classes and standard. Added manage_users controller

--- controllers/classes.py
+++ controllers/classes.py
@@ -3,9 +3,9 @@
 
 def index():
     classes = SQLFORM.smartgrid(db.classes)
-    return locals()
+    return dict(classes=classes)
 
 
 def create():
-    form = SQLFORM(db.classes).process(next=URL('index'))
+    form = SQLFORM(db.classes, submit_button='Create', labels = {'gradeLevel': 'Grade Level', 'startDate': 'Start Date', 'endDate': 'End Date'} ).process(next=URL('index'))
     return dict(form=form)
