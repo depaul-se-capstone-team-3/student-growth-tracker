487156dd73bdaa26e5bf93573944cc956f31504d
Bryan Patzke <projects@2bpencil.com>
2015-10-20 07:55:14 -0500

    Basic layout started for class list.

--- controllers/classes.py
+++ controllers/classes.py
@@ -3,17 +3,22 @@
 
 @auth.requires_login()
 def index():
-    query = ((db.gradebook.teacher==auth.user_id) &
-             (db.gradebook.classes==db.classes.id) &
-             (db.classes.content_area==db.contentarea.id)
-    )
-    rows = db(query).select()#db.auth_user.first_name,
-                            # db.auth_user.last_name,
-                            # db.classes.name,
-                            # db.contentarea.name)
-    return dict(rows=rows)
+    class_query = ((db.gradebook.teacher==auth.user_id) &
+                   (db.gradebook.classes==db.classes.id) &
+                   (db.classes.content_area==db.contentarea.id))
+    class_rows = db(class_query).select()
+
+    class_roster_query = ((db.gradebook.teacher==auth.user_id) &
+                          (db.gradebook.classes==db.classes.id) &
+                          (db.student_classes.student_id==db.student.id) &
+                          (db.student.user_id==db.auth_user.id))
+    class_roster = db(class_roster_query).select()
+    return dict(rows=class_rows, class_roster=class_roster)
 
 
 def create():
-    form = SQLFORM(db.classes, submit_button='Create', labels = {'gradeLevel': 'Grade Level', 'startDate': 'Start Date', 'endDate': 'End Date'} ).process(next=URL('index'))
+    form = SQLFORM(db.classes, submit_button='Create',
+                   labels = {'gradeLevel': 'Grade Level',
+                             'startDate': 'Start Date',
+                             'endDate': 'End Date'} ).process(next=URL('index'))
     return dict(form=form)
