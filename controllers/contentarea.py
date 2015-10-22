79b06cd178caa5fb6adfc8bd71d19c1eac137745
Larry Graves <lawrence.graves86@gmail.com>
2015-10-21 19:35:26 -0500

    added docstrings to contentarea and gradebook controllers, and reimplemented filtering behavior on gradebook controller

--- controllers/contentarea.py
+++ controllers/contentarea.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 # try something like
 def index():
+    """basic index, return grid based on available contentareas"""
     grid = SQLFORM.smartgrid(db.contentarea)
     return locals()
 
 def create():
+    """basic create, makes form for new contentarea and redirect to index"""
     form = SQLFORM(db.contentarea).process(next=URL('index'))
     return dict(form=form)
--- controllers/gradebook.py
+++ controllers/gradebook.py
@@ -1,20 +1,15 @@
 # -*- coding: utf-8 -*-
 # try something like
 @auth.requires_login()
-#def index():
-#    query = db.gradebook.teacher == auth.user_id
-#    gradebook = db(query)
-#    return dict(message='Class List - %(first_name)s' % auth.user,gradebook=gradebook)
-@auth.requires_login()
 def index():
+    """pull up teacher and classes that match current user and return a grid with the result"""
     constraints = db.gradebook.teacher == auth.user.id
-    # grid = SQLFORM(db.gradebook).process()
-    grid = db(db.gradebook.teacher).select(join=db.gradebook.on((db.gradebook.teacher==db.auth_user.id) &
-                                                                (db.gradebook.classes==db.classes.id)))
-    # grid = SQLFORM.smartgrid(db.gradebook,constraints={'gradebook':(db.gradebook.teacher==auth.user_id)})
+    grid = db(constraints).select(join=db.gradebook.on(
+        (db.gradebook.teacher==db.auth_user.id) & (db.gradebook.classes==db.classes.id)))
     # response.flash = 'Class List - %(first_name)s' % auth.user
     return dict(grid=grid)
 
 def create():
+    """generate form for new gradebook entry, redirect to index"""
     form = SQLFORM(db.gradebook).process(next=URL('index'))
     return dict(form=form)
