135df8bd088f3319b91438149ed4043cc4b31577
Bryan Patzke <projects@2bpencil.com>
2015-10-22 19:22:13 -0500

    Cleaning up to merge upstream.

--- controllers/classes.py
+++ controllers/classes.py
@@ -20,14 +20,21 @@ def index():
     .. todo:: Move the queries into either the ``db_01_general.py`` script, or
               the ``db_30_classes.py`` script.
     """
+
+    # Get the list of classes.
+    # Get the list of students in each class
+    # Get the list of assignments for each student in the class.
+    # Is there a way to do this in one query, which I then break up into a set
+    # of dictionaries?
+    # Or is it just easier to make objects that know how to deal with these items
+    # themselves?
+
     class_query = ((db.gradebook.teacher==auth.user_id) &
                    (db.gradebook.classes==db.classes.id) &
                    (db.classes.content_area==db.contentarea.id))
     class_rows = db(class_query).select()
 
-    # class_roster = db().select(db.student.ALL,
-    #                            db.student_grade.ALL,
-    #                            left=db.student_grade.on(db.student.id==db.student_grade.student_id))
+
     class_roster_query = ((db.gradebook.teacher==auth.user_id) &
                           (db.gradebook.classes==db.classes.id) &
                           (db.student_classes.student_id==db.student.id) &
