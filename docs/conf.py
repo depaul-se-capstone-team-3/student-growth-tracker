bee6ec9d66bcf6799559c7bc4a1ea089299fc290
Bryan Patzke <bryan.patzke@gmail.com>
2015-11-03 12:21:20 -0600

    Refactor classes.py controller.

--- docs/conf.py
+++ docs/conf.py
@@ -292,4 +292,5 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {'python': ('https://docs.python.org/2.7', None),
+                       'web2py': ('http://web2py.readthedocs.org/en/latest/', None)}
--- models/db_02_functions.py
+++ models/db_02_functions.py
@@ -3,6 +3,7 @@ Put things here that will be used in multiple places.
 """
 
 def teacher_classes_query(teacher_id, class_id=None):
+    """Return a """
     query = ((db.gradebook.teacher==teacher_id) &
              (db.gradebook.classes==db.classes.id) &
              (db.classes.content_area==db.contentarea.id))
@@ -45,21 +46,21 @@ def get_class_assignments(teacher_id, class_id):
     return class_assignments
 
 def get_student_assignments(teacher_id, class_id):
-    assignment_query = teacher_classes_query(teacher_id, class_id)
-    assignment_query &= (db.classes.id==db.student_classes.class_id)   # Add the students
-    assignment_query &= (db.student_classes.student_id==db.student.id) # via the mapping table.
-    assignment_query &= (db.student.user_id==db.auth_user.id)          # Get details from auth_user.
-    assignment_query &= (db.student.id==db.student_grade.student_id)   # Find the grades for each student
-    assignment_query &= (db.student_grade.grade_id==db.grade.id)       # via the mapping table.
+    query = (teacher_classes_query(teacher_id, class_id) &
+             (db.classes.id==db.student_classes.class_id) &
+             (db.student_classes.student_id==db.student.id) &
+             (db.student.user_id==db.auth_user.id) &
+             (db.student.id==db.student_grade.student_id) &
+             (db.student_grade.grade_id==db.grade.id))
 
-    assignment_results = db(assignment_query).select(db.student.id,
-                                                     db.auth_user.first_name,
-                                                     db.auth_user.last_name,
-                                                     db.grade.name,
-                                                     db.student_grade.student_score,
-                                                     db.student_grade.id,
-                                                     orderby=[db.student.id,
-                                                              db.grade.due_date])
+    results = db(query).select(db.student.id,
+                               db.auth_user.first_name,
+                               db.auth_user.last_name,
+                               db.grade.name,
+                               db.student_grade.student_score,
+                               db.student_grade.id,
+                               orderby=[db.student.id,
+                                        db.grade.due_date])
 
     anames = ['', '']
     for a in get_class_assignments(teacher_id, class_id):
@@ -68,7 +69,7 @@ def get_student_assignments(teacher_id, class_id):
     assignments = [anames]
 
     for student in get_class_roster(teacher_id, class_id):
-        grades = assignment_results.find(lambda s: s.student.id==student[0])
+        grades = results.find(lambda s: s.student.id==student[0])
         for grade in grades:
             student.append(grade.student_grade.student_score)
         assignments.append(student)
