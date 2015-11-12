2fed87dd1e129e7a1c61df25ff874102d841f885
Bryan Patzke <bryan.patzke@gmail.com>
2015-11-12 08:38:31 -0600

    Interface cleanup.

--- models/db_01_general.py
+++ models/db_01_general.py
@@ -5,5 +5,7 @@
 IS_PHONE_NUMBER = IS_MATCH('^1?((-)\d{3}-?|\(d{3}\))\d{3}-?\d{4}$')
 """This validator matches a US phone number."""
 
+DATE_FORMAT = '%Y-%m-%d'
+
 if __name__ == '__main__':
     pass
