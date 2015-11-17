714a603927d5716a66f1e9960b52aaa1ae7dd90d
Bryan Patzke <projects@2bpencil.com>
2015-11-16 20:03:23 -0600

    Updated classes menu.
    The ``classes`` menu is not populated based on the role of the user
    logged in.

--- models/menu_20_classes.py
+++ models/menu_20_classes.py
@@ -1,5 +1,6 @@
+classes_menu_items = []
+
 if auth.has_membership('Teacher'):
-    classes_menu_items = []
 
     class_list = db((db.gradebook.teacher==auth.user_id) &
                     (db.gradebook.classes==db.classes.id)).select(db.classes.id,
@@ -8,6 +9,20 @@ if auth.has_membership('Teacher'):
     for class_ in class_list:
         classes_menu_items.append((class_.name, False,
                                    URL('classes', 'overview', args=[class_.id]), []))
+
+    response.menu += [(T('Classes'), False, '', classes_menu_items)]
+
+if auth.has_membership('Student'):
+
+    class_list = db((db.student.user_id==auth.user_id) &
+                    (db.student_classes.student_id==db.student.id) &
+                    (db.student_classes.class_id==db.classes.id)).select(db.classes.id,
+                                                                         db.classes.name)
+
+    for class_ in class_list:
+        classes_menu_items.append((class_.name, False,
+                                   URL('students', 'index',
+                                       args=[auth.user_id, class_.id]), []))
 
     response.menu += [(T('Classes'), False, '', classes_menu_items)]
 
