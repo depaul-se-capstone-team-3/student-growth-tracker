f4f1551122afce190be4cc50f7d94b340e1dc394
Allan Guan <forevaufo@gmail.com>
2016-01-06 12:29:10 -0600

    fixed unittest stuff

--- tests/controllers/GradeBookController.py
+++ tests/controllers/GradeBookController.py
@@ -8,16 +8,6 @@ class GradeBookController(unittest.TestCase):
 		self.request = current.request
 		self.controller = execfile("applications/student_growth_tracker/controllers/gradebook.py", globals())
 		db = test_db
-		print("-------print1--------------------------------")
-		print(db(db.auth_group).isempty())
-		print("-------print1--------------------------------")
-		auth.add_group(ADMIN, ADMIN)
-                auth.add_group(TEACHER, TEACHER)
-                auth.add_group(STUDENT, STUDENT)
-                auth.add_group(PARENT, PARENT)
-                print("-------print2--------------------------------")
-                print(db(db.auth_group).isempty())
-                print("-------print2--------------------------------")
 
 	def test_gradebook(self):
 		self.assertEqual('boo'.upper(), 'BOO')
