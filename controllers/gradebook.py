81482c4bcaae45cb4128da590505c28fbd5e4e49
Joseph-Richard <JosephTRichard@gmail.com>
2016-02-11 11:17:48 -0600

    removed some stuff from gradebook overview function.

--- controllers/gradebook.py
+++ controllers/gradebook.py
@@ -44,13 +44,15 @@ def index():
 
 # This should go under manage - assuming this is a school/district maanged tool,
 # teachers won't add classes to their gradebooks.
+@auth.requires_login()
 def create():
     """generate form for new gradebook entry, redirect to index"""
     form = SQLFORM(db.gradebook).process(next=URL('index'))
     return dict(form=form)
 
-
+@auth.requires_login()
 def overview():
+    '''THIS METHOD IS NOT DOING ANYTHING RIGHT NOW. CONSIDER DELETING?'''
     if auth.has_membership(2, auth.user_id):
         pass
     else:
@@ -84,9 +86,6 @@ def overview():
         total_grades = db(grade_query).count()
         #get averages and set up the outer dictionary.
         average = round(total_score / total_possible * 100.0, 2)
-###########
-       # create_single_class_pdf(teacher_name, class_id, class_name, average, total_students, total_grades, table_grid)
-        
         standards = get_standards_for_class(cid)
         total_score =0
         students_score = 0
@@ -114,126 +113,12 @@ def overview():
                 for score in scores:
                     students_score = students_score + score.student_score
                     j = j+1
-                
+
                 total_score = total_score *j
                 assignment_average = (students_score/total_score)*100
                 assignment = grade_name+": "+assignment_average
                 standard_table[i].append(assignment)
             i = i+1
 
-        
-        
-        
-        '''
-    #To display standards
-        query = ((db.classes.id==class_id) &
-                     (db.class_grade.class_id==class_id) &
-                     (db.class_grade.grade_id==db.grade.id) &
-                     (db.student_grade.grade_id==db.grade.id) &
-                     (db.grade.id==db.grade_standard.grade_id) &
-                     (db.standard.id==db.grade_standard.standard_id) &
-                     (db.standard.content_area == db.contentarea.id))
-
-        standard_list = db(query).select(db.standard.id, db.standard.short_name, db.standard.reference_number,db.student_grade.student_score, db.grade.score)
-
-        standard_dict = {}
-        for row in standard_list:
-            if row.standard.id in standard_dict.keys():
-                if((row.grade.score != 0.0) | (row.student_grade.student_score != 0.0)):
-                    max_score = standard_dict[row.standard.id][0] + row.grade.score
-                    student_score = standard_dict[row.standard.id][1] + row.student_grade.student_score
-                    standard_dict[row.standard.id] = [max_score, student_score, row.standard.reference_number, row.standard.short_name]
-
-            else:
-                standard_dict[row.standard.id] = [row.grade.score, row.student_grade.student_score, row.standard.reference_number, row.standard.short_name]
-                standard_grid = get_student_assignments(teacher_id,class_id,row.standard.id)'''
-        create_single_class_pdf(teacher_name, class_id, class_name, average, total_students, total_grades, standard_table)
-
-    #build_table_pdf()
-    #create_a_test_pdf(class_names, class_averages_dict, classes_dict, standards_dict)
-    #create_single_class_pdf(class_id, class_name, class_average, standard_dict)
     return dict(class_names=class_names,
                 class_averages_dict=class_averages_dict)
-
-
-    
-def create_single_class_pdf(teacher_name, class_id,class_name, class_average, total_students, total_grades, table_grid):
-    doc = SimpleDocTemplate("single_class.pdf",pagesize=letter,rightMargin=72,leftMargin=72,topMargin=72,bottomMargin=18)
-    Story=[]
-    formatted_time = time.ctime()
-    styles=getSampleStyleSheet()
-    styles.add(ParagraphStyle(name='Justify', alignment=TA_JUSTIFY))
-    styles.add(ParagraphStyle(name='Indent', rightIndent=3))
-    ptext = '<font size=12>%s</font>' % formatted_time
-
-    Story.append(Paragraph(ptext, styles["Normal"]))
-    Story.append(Spacer(1, 12))
-
-    #Teacher Name
-    ptext='<font size=12><b>%s %s</b></font>'%(teacher_name.first_name, teacher_name.last_name)
-    Story.append(Paragraph(ptext, styles["Justify"]))
-    Story.append(Spacer(1, 12))
-
-    #Class Name
-    ptext = '<font size=12>%s: </font>'%(class_name)
-    Story.append(Paragraph(ptext, styles["Justify"]))
-    Story.append(Spacer(1, 7))
-
-    #Class Average
-    ptext = '<font size=12>Class Average:%s%%</font>'%(class_average)
-    Story.append(Paragraph(ptext, styles["Justify"]))
-    Story.append(Spacer(1,12))
-    
-    #Total Students
-    ptext = '<font size=12>Total Students:%s</font>'%(total_students)
-    Story.append(Paragraph(ptext, styles["Justify"]))
-    Story.append(Spacer(1,12))
-    
-    #Total Assignments
-    ptext = '<font size=12>Total Assignments:%s</font>'%(total_grades)
-    Story.append(Paragraph(ptext, styles["Justify"]))
-    Story.append(Spacer(1,12))
-    Story.append(Spacer(1,40))
-
-    #Standards Data goes here:
-    ptext = '<font size=18>--STANDARD DATA GOES HERE--</font>'
-    Story.append(Paragraph(ptext, styles["Justify"]))
-    Story.append(Spacer(1,40))
-
-    #Standards Table
-    t=Table(table_grid)
-    t.setStyle(t.setStyle(TableStyle([('BOX', (0,0), (-1,-1), 0.25, colors.black),
-                                      ('INNERGRID', (0,0), (-1,-1), 0.25, colors.black),])))
-    Story.append(t)
-
-
-
-    doc.build(Story)
-def build_table_pdf(): 
-    doc = SimpleDocTemplate("complex_cell_values.pdf", pagesize=letter)
-    # container for the 'Flowable' objects
-    elements = []
- 
-    styleSheet = getSampleStyleSheet()
-
-    P0 = Paragraph('''
-               <b>A pa<font color=red>r</font>a<i>graph</i></b>
-               <super><font color=yellow>1</font></super>''',
-               styleSheet["BodyText"])
-    P = Paragraph('''
-    <para align=center spaceb=3>The <b>ReportLab Left
-    <font color=red>Logo</font></b>
-    Image</para>''',
-    styleSheet["BodyText"])
-    data= [['A', 'B', 'C', P0, 'D'],
-       ['00', '01', '02', '04'],
-       ['10', '11', '12', '14'],
-       ['20', '21', '22', '23', '24'],
-       ['30', '31', '32', '33', '34']]
- 
-    t=Table(data,style=['Normal'])
-    t._argW[3]=1.5*inch
- 
-    elements.append(t)
-    # write the document to disk
-    doc.build(elements)
